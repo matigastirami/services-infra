52367fb15318b2acde7ddf9e51055855c2f3e7dd
Matías Ramirez <matigastirami@gmail.com>
2023-08-15 09:48:42 -0300

    chore: files formatting

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "instance_name" {
   description = "Tag name for the EC2 instance"
-  type = string
-  default = 'ExampleEC2Instance'
+  type        = string
+  default     = "ExampleEC2Instance"
 }
