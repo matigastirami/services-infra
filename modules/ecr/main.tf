685a1d5c1c34cf1ae7dd4123c58c76dfaec24fbf
Matías Ramirez <matigastirami@gmail.com>
2023-09-26 16:51:05 -0300

    chore: ecr repos

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,4 +1,11 @@
-resource "aws_ecr_repository" "users-service-repo" {
-  name = "users-service"
-  tags = {}
+# Create an ECR repository for Docker images
+resource "aws_ecr_repository" "users_service" {
+  name = "users-service" # Replace with your desired name
+  image_tag_mutability = "MUTABLE" # You can choose "MUTABLE" or "IMMUTABLE" as needed
+}
+
+# Create an ECR repository for Helm charts/packages
+resource "aws_ecr_repository" "users_service_chart" {
+  name = "users_service_chart" # Replace with your desired name
+  image_tag_mutability = "IMMUTABLE" # You can choose "MUTABLE" or "IMMUTABLE" as needed
 }
